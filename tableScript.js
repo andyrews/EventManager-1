9582888d568a036f2c0722706981b69af447c826
André <60563950+andrexxrojas@users.noreply.github.com>
2024-03-19 03:46:17 +0800

    Can now add groups

--- tableScript.js
+++ tableScript.js
@@ -1,18 +1,45 @@
-//For toggling groups
 let groups = document.querySelectorAll(".group")
-// let taskList = groups.querySelector(".taskList")
 
-window.onload = function(){
-    for(let i = 0; i < groups.length; i++){
-        let group = groups[i]
-        let taskList = group.querySelector(".taskList")
-    
-        group.addEventListener("click", function(){
-            if(taskList.style.display === "none"){
-                taskList.style.display = "block"
-            }else{
-                taskList.style.display = "none";
-            }
-        })
+//For adding new group
+let groupContainer = document.querySelector(".groupTask")
+let groupTitle = document.querySelector("#groupTitleText")
+let addGroupBtn = document.querySelector(".addGroupBtn")
+
+addGroupBtn.addEventListener("click", function(){
+    let newGroupTitle = groupTitle.value;
+
+    if(newGroupTitle === ""){
+        return
+    }else{
+
+        //For entire Group
+        let group = document.createElement("div")
+        group.classList = "group"
+
+        //For Group Box
+        let groupTitleDiv = document.createElement("div")
+        groupTitleDiv.classList = "groupTitle"
+
+        let icon = document.createElement("i")
+        icon.classList = "fa-solid fa-angle-right"
+
+        let title = document.createElement("p")
+        title.innerHTML = newGroupTitle
+
+        groupTitleDiv.appendChild(icon)
+        groupTitleDiv.appendChild(title)
+        
+        group.appendChild(groupTitleDiv)
+        
+        groupContainer.appendChild(group)
+
+        updateGroupsArray();
     }
+
+    console.log(groups);
+})
+
+function updateGroupsArray(){
+    groups = document.querySelectorAll(".group")
 }
+
